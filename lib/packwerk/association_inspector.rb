e22c9626f41411ff8046272ff37df7dfdebc5b60
Frederik Dudzik <frederik.dudzik@shopify.com>
2020-10-27 14:44:44 -0700

    add private method to retrieve association_name
    I'm moving a chunk of code out of the constant_name_from_node method.
    This makes the intend clearer a bit clearer.

--- lib/packwerk/association_inspector.rb
+++ lib/packwerk/association_inspector.rb
@@ -26,14 +26,11 @@ module Packwerk
       return unless association?(node)
 
       arguments = Node.method_arguments(node)
-      association_name = Node.literal_value(arguments[0]) if Node.symbol?(arguments[0])
-      return nil unless association_name
+      return unless (association_name = association_name(arguments))
 
-      association_options = arguments.detect { |n| Node.hash?(n) }
-      class_name_node = Node.value_from_hash(association_options, :class_name) if association_options
-
-      if class_name_node
-        Node.literal_value(class_name_node) if Node.string?(class_name_node)
+      if (class_name_node = custom_class_name(arguments))
+        return unless Node.string?(class_name_node)
+        Node.literal_value(class_name_node)
       else
         @inflector.classify(association_name.to_s)
       end
@@ -44,6 +41,19 @@ module Packwerk
     def association?(node)
       method_name = Node.method_name(node)
       @associations.include?(method_name)
+    end
+
+    def custom_class_name(arguments)
+      association_options = arguments.detect { |n| Node.hash?(n) }
+      return unless association_options
+
+      Node.value_from_hash(association_options, :class_name)
+    end
+
+    def association_name(arguments)
+      return unless Node.symbol?(arguments[0])
+
+      Node.literal_value(arguments[0])
     end
   end
 end
