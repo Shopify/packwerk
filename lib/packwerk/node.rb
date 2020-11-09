2aee02a2e514080c3938389364d1787785f24f6c
Frederik Dudzik <frederik.dudzik@shopify.com>
2020-11-09 13:08:55 -0800

    rename type to type_of

--- lib/packwerk/node.rb
+++ lib/packwerk/node.rb
@@ -10,7 +10,7 @@ module Packwerk
 
     class << self
       def class_or_module_name(class_or_module_node)
-        case type(class_or_module_node)
+        case type_of(class_or_module_node)
         when CLASS, MODULE
           # (class (const nil :Foo) (const nil :Bar) (nil))
           #   "class Foo < Bar; end"
@@ -24,7 +24,7 @@ module Packwerk
       end
 
       def constant_name(constant_node)
-        case type(constant_node)
+        case type_of(constant_node)
         when CONSTANT_ROOT_NAMESPACE
           ""
         when CONSTANT, CONSTANT_ASSIGNMENT
@@ -62,19 +62,19 @@ module Packwerk
       end
 
       def enclosing_namespace_path(starting_node, ancestors:)
-        ancestors.select { |n| [CLASS, MODULE].include?(type(n)) }
+        ancestors.select { |n| [CLASS, MODULE].include?(type_of(n)) }
           .each_with_object([]) do |node, namespace|
           # when evaluating `class Child < Parent`, the const node for `Parent` is a child of the class
           # node, so it'll be an ancestor, but `Parent` is not evaluated in the namespace of `Child`, so
           # we need to skip it here
-          next if type(node) == CLASS && parent_class(node) == starting_node
+          next if type_of(node) == CLASS && parent_class(node) == starting_node
 
           namespace.prepend(class_or_module_name(node))
         end
       end
 
       def literal_value(string_or_symbol_node)
-        case type(string_or_symbol_node)
+        case type_of(string_or_symbol_node)
         when STRING, SYMBOL
           # (str "foo")
           #   "'foo'"
@@ -92,31 +92,31 @@ module Packwerk
       end
 
       def constant?(node)
-        type(node) == CONSTANT
+        type_of(node) == CONSTANT
       end
 
       def constant_assignment?(node)
-        type(node) == CONSTANT_ASSIGNMENT
+        type_of(node) == CONSTANT_ASSIGNMENT
       end
 
       def class?(node)
-        type(node) == CLASS
+        type_of(node) == CLASS
       end
 
       def method_call?(node)
-        type(node) == METHOD_CALL
+        type_of(node) == METHOD_CALL
       end
 
       def hash?(node)
-        type(node) == HASH
+        type_of(node) == HASH
       end
 
       def string?(node)
-        type(node) == STRING
+        type_of(node) == STRING
       end
 
       def symbol?(node)
-        type(node) == SYMBOL
+        type_of(node) == SYMBOL
       end
 
       def method_arguments(method_call_node)
@@ -136,7 +136,7 @@ module Packwerk
       end
 
       def module_name_from_definition(node)
-        case type(node)
+        case type_of(node)
         when CLASS, MODULE
           # "class My::Class; end"
           # "module My::Module; end"
@@ -146,7 +146,7 @@ module Packwerk
           # "My::Module = ..."
           rvalue = node.children.last
 
-          case type(rvalue)
+          case type_of(rvalue)
           when METHOD_CALL
             # "Class.new"
             # "Module.new"
@@ -169,7 +169,7 @@ module Packwerk
       end
 
       def parent_class(class_node)
-        raise TypeError unless type(class_node) == CLASS
+        raise TypeError unless type_of(class_node) == CLASS
 
         # (class (const nil :Foo) (const nil :Bar) (nil))
         #   "class Foo < Bar; end"
@@ -178,7 +178,7 @@ module Packwerk
 
       def parent_module_name(ancestors:)
         definitions = ancestors
-          .select { |n| [CLASS, MODULE, CONSTANT_ASSIGNMENT, BLOCK].include?(type(n)) }
+          .select { |n| [CLASS, MODULE, CONSTANT_ASSIGNMENT, BLOCK].include?(type_of(n)) }
 
         names = definitions.map do |definition|
           name_part_from_definition(definition)
@@ -212,12 +212,12 @@ module Packwerk
         :MODULE, :STRING, :SYMBOL,
       )
 
-      def type(node)
+      def type_of(node)
         node.type
       end
 
       def hash_pair_key(hash_pair_node)
-        raise TypeError unless type(hash_pair_node) == HASH_PAIR
+        raise TypeError unless type_of(hash_pair_node) == HASH_PAIR
 
         # (pair (int 1) (int 2))
         #   "1 => 2"
@@ -227,7 +227,7 @@ module Packwerk
       end
 
       def hash_pair_value(hash_pair_node)
-        raise TypeError unless type(hash_pair_node) == HASH_PAIR
+        raise TypeError unless type_of(hash_pair_node) == HASH_PAIR
 
         # (pair (int 1) (int 2))
         #   "1 => 2"
@@ -241,11 +241,11 @@ module Packwerk
 
         # (hash (pair (int 1) (int 2)) (pair (int 3) (int 4)))
         #   "{1 => 2, 3 => 4}"
-        hash_node.children.select { |n| type(n) == HASH_PAIR }
+        hash_node.children.select { |n| type_of(n) == HASH_PAIR }
       end
 
       def method_call_node(block_node)
-        raise TypeError unless type(block_node) == BLOCK
+        raise TypeError unless type_of(block_node) == BLOCK
 
         # (block (send (lvar :foo) :bar) (args) (int 42))
         #   "foo.bar do 42 end"
@@ -269,7 +269,7 @@ module Packwerk
       end
 
       def name_part_from_definition(node)
-        case type(node)
+        case type_of(node)
         when CLASS, MODULE, CONSTANT_ASSIGNMENT
           module_name_from_definition(node)
         when BLOCK
@@ -278,7 +278,7 @@ module Packwerk
       end
 
       def receiver(method_call_or_block_node)
-        case type(method_call_or_block_node)
+        case type_of(method_call_or_block_node)
         when METHOD_CALL
           method_call_or_block_node.children[0]
         when BLOCK
