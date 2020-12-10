dcb8d836a96c37b0bc392f05707e3cca0de7cb44
Samuel Giles <sam@samuelgil.es>
2020-12-10 13:52:21 +0000

    Move to typed: strict on implementations of Packwerk::Checker
    Fixes nil returns where return type was T::Boolean along the way

--- lib/packwerk/dependency_checker.rb
+++ lib/packwerk/dependency_checker.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "packwerk/violation_type"
@@ -6,20 +6,28 @@ require "packwerk/checker"
 
 module Packwerk
   class DependencyChecker
+    extend T::Sig
     include Checker
 
+    sig { override.returns(ViolationType) }
     def violation_type
       ViolationType::Dependency
     end
 
+    sig do
+      override
+        .params(reference: Packwerk::Reference, reference_lister: Packwerk::ReferenceLister)
+        .returns(T::Boolean)
+    end
     def invalid_reference?(reference, reference_lister)
-      return unless reference.source_package
-      return unless reference.source_package.enforce_dependencies?
-      return if reference.source_package.dependency?(reference.constant.package)
-      return if reference_lister.listed?(reference, violation_type: violation_type)
+      return false unless reference.source_package
+      return false unless reference.source_package.enforce_dependencies?
+      return false if reference.source_package.dependency?(reference.constant.package)
+      return false if reference_lister.listed?(reference, violation_type: violation_type)
       true
     end
 
+    sig { override.params(reference: Packwerk::Reference).returns(String) }
     def message_for(reference)
       "Dependency violation: #{reference.constant.name} belongs to '#{reference.constant.package}', but " \
         "'#{reference.source_package}' does not specify a dependency on " \
--- lib/packwerk/privacy_checker.rb
+++ lib/packwerk/privacy_checker.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 require "packwerk/violation_type"
@@ -6,26 +6,34 @@ require "packwerk/checker"
 
 module Packwerk
   class PrivacyChecker
+    extend T::Sig
     include Checker
 
+    sig { override.returns(Packwerk::ViolationType) }
     def violation_type
       ViolationType::Privacy
     end
 
+    sig do
+      override
+        .params(reference: Packwerk::Reference, reference_lister: Packwerk::ReferenceLister)
+        .returns(T::Boolean)
+    end
     def invalid_reference?(reference, reference_lister)
-      return if reference.constant.public?
+      return false if reference.constant.public?
 
       privacy_option = reference.constant.package.enforce_privacy
-      return if enforcement_disabled?(privacy_option)
+      return false if enforcement_disabled?(privacy_option)
 
-      return unless privacy_option == true ||
+      return false unless privacy_option == true ||
         explicitly_private_constant?(reference.constant, explicitly_private_constants: privacy_option)
 
-      return if reference_lister.listed?(reference, violation_type: violation_type)
+      return false if reference_lister.listed?(reference, violation_type: violation_type)
 
       true
     end
 
+    sig { override.params(reference: Packwerk::Reference).returns(String) }
     def message_for(reference)
       source_desc = reference.source_package ? "'#{reference.source_package}'" : "here"
       "Privacy violation: '#{reference.constant.name}' is private to '#{reference.constant.package}' but " \
@@ -35,12 +43,22 @@ module Packwerk
 
     private
 
+    sig do
+      params(
+        constant: ConstantDiscovery::ConstantContext,
+        explicitly_private_constants: T::Array[String]
+      ).returns(T::Boolean)
+    end
     def explicitly_private_constant?(constant, explicitly_private_constants:)
       explicitly_private_constants.include?(constant.name) ||
         # nested constants
         explicitly_private_constants.any? { |epc| constant.name.start_with?(epc + "::") }
     end
 
+    sig do
+      params(privacy_option: T.nilable(T.any(T::Boolean, T::Array[String])))
+        .returns(T::Boolean)
+    end
     def enforcement_disabled?(privacy_option)
       [false, nil].include?(privacy_option)
     end
