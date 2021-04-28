bc88ef125729a651ab676686ad32c9bbd47aef96
Matt Clark <44023+mclark@users.noreply.github.com>
2021-04-27 20:25:32 -0400

    normalize parse commands into single class
    When we parallelize packwerk, the command/parse run will be responsible
    for more processing. Normalizing repeated functionality now will
    simplify future changes.

--- lib/packwerk/cli.rb
+++ lib/packwerk/cli.rb
@@ -12,11 +12,8 @@ require "packwerk/formatters/progress_formatter"
 require "packwerk/inflector"
 require "packwerk/output_style"
 require "packwerk/output_styles/plain"
-require "packwerk/run_context"
 require "packwerk/updating_deprecated_references"
 require "packwerk/checking_deprecated_references"
-require "packwerk/commands/detect_stale_violations_command"
-require "packwerk/commands/update_deprecations_command"
 require "packwerk/commands/offense_progress_marker"
 
 module Packwerk
@@ -26,22 +23,17 @@ module Packwerk
 
     sig do
       params(
-        run_context: T.nilable(Packwerk::RunContext),
         configuration: T.nilable(Configuration),
         out: T.any(StringIO, IO),
         err_out: T.any(StringIO, IO),
         style: Packwerk::OutputStyle
       ).void
     end
-    def initialize(run_context: nil, configuration: nil, out: $stdout, err_out: $stderr, style: OutputStyles::Plain.new)
+    def initialize(configuration: nil, out: $stdout, err_out: $stderr, style: OutputStyles::Plain.new)
       @out = out
       @err_out = err_out
       @style = style
       @configuration = configuration || Configuration.from_path
-      @run_context = run_context || Packwerk::RunContext.from_configuration(
-        @configuration,
-        reference_lister: ::Packwerk::CheckingDeprecatedReferences.new(@configuration.root_path),
-      )
       @progress_formatter = Formatters::ProgressFormatter.new(@out, style: style)
     end
 
@@ -60,13 +52,13 @@ module Packwerk
       when "generate_configs"
         generate_configs
       when "check"
-        check(args)
+        output_result(parse_run(args).check)
       when "detect-stale-violations"
-        detect_stale_violations(args)
+        output_result(parse_run(args).detect_stale_violations)
       when "update"
         update(args)
       when "update-deprecations"
-        update_deprecations(args)
+        output_result(parse_run(args).update_deprecations)
       when "validate"
         validate(args)
       when nil, "help"
@@ -143,54 +135,10 @@ module Packwerk
 
     def update(paths)
       warn("`packwerk update` is deprecated in favor of `packwerk update-deprecations`.")
-      update_deprecations(paths)
+      output_result(parse_run(paths).update_deprecations)
     end
 
-    def update_deprecations(paths)
-      update_deprecations = Commands::UpdateDeprecationsCommand.new(
-        files: fetch_files_to_process(paths),
-        configuration: @configuration,
-        offenses_formatter: offenses_formatter,
-        progress_formatter: @progress_formatter
-      )
-      result = update_deprecations.run
-      @out.puts
-      @out.puts(result.message)
-      result.status
-    end
-
-    def check(paths)
-      files = fetch_files_to_process(paths)
-
-      @progress_formatter.started(files)
-
-      all_offenses = T.let([], T.untyped)
-      execution_time = Benchmark.realtime do
-        files.each do |path|
-          @run_context.process_file(file: path).tap do |offenses|
-            mark_progress(offenses: offenses, progress_formatter: @progress_formatter)
-            all_offenses.concat(offenses)
-          end
-        end
-      rescue Interrupt
-        @out.puts
-        @out.puts("Manually interrupted. Violations caught so far are listed below:")
-      end
-
-      @progress_formatter.finished(execution_time)
-      @out.puts
-      @out.puts(offenses_formatter.show_offenses(all_offenses))
-
-      all_offenses.empty?
-    end
-
-    def detect_stale_violations(paths)
-      detect_stale_violations = Commands::DetectStaleViolationsCommand.new(
-        files: fetch_files_to_process(paths),
-        configuration: @configuration,
-        progress_formatter: @progress_formatter
-      )
-      result = detect_stale_violations.run
+    def output_result(result)
       @out.puts
       @out.puts(result.message)
       result.status
@@ -238,6 +186,15 @@ module Packwerk
       else
         false
       end
+    end
+
+    def parse_run(paths)
+      ParseRun.new(
+        files: fetch_files_to_process(paths),
+        configuration: @configuration,
+        progress_formatter: @progress_formatter,
+        offenses_formatter: offenses_formatter
+      )
     end
 
     def offenses_formatter
---
+++ test/unit/parse_run_test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+require "test_helper"
+require "rails_test_helper"
+require "packwerk/parse_run"
+
+module Packwerk
+  class ParseRunTest < Minitest::Test
+    test "#detect_stale_violations returns expected Result when stale violations present" do
+      DetectStaleDeprecatedReferences.any_instance.stubs(:stale_violations?).returns(true)
+      RunContext.any_instance.stubs(:process_file).returns([])
+
+      parse_run = Packwerk::ParseRun.new(files: ["path/of/exile.rb"], configuration: Configuration.from_path)
+      result = parse_run.detect_stale_violations
+      assert_equal "There were stale violations found, please run `packwerk update-deprecations`", result.message
+      refute result.status
+    end
+
+    test "#update_deprecations returns success when there are no offenses" do
+      RunContext.any_instance.stubs(:process_file).returns([])
+      UpdatingDeprecatedReferences.any_instance.expects(:dump_deprecated_references_files).once
+
+      parse_run = Packwerk::ParseRun.new(files: ["path/of/exile.rb"], configuration: Configuration.from_path)
+      result = parse_run.update_deprecations
+
+      assert_equal result.message, <<~EOS
+        No offenses detected 🎉
+        ✅ `deprecated_references.yml` has been updated.
+      EOS
+      assert result.status
+    end
+
+    test "#update_deprecations returns exit code 1 when there are offenses" do
+      offense = Offense.new(file: "path/of/exile.rb", message: "something")
+      RunContext.any_instance.stubs(:process_file).returns([offense])
+      UpdatingDeprecatedReferences.any_instance.expects(:dump_deprecated_references_files).once
+
+      parse_run = Packwerk::ParseRun.new(files: ["path/of/exile.rb"], configuration: Configuration.from_path)
+      result = parse_run.update_deprecations
+
+      assert_equal result.message, <<~EOS
+        path/of/exile.rb
+        something
+
+        1 offense detected
+
+        ✅ `deprecated_references.yml` has been updated.
+      EOS
+      refute result.status
+    end
+  end
+end
