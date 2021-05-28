9c809dbd916392272b52e672e194611ffa6f50f6
Matt Clark <44023+mclark@users.noreply.github.com>
2021-05-28 17:12:50 -0400

    return arrays of offenses for each file & constant
    Each processed constant can result in up to at least two violations
    and each file can have innumerable constant violations. Now all violations
    present in the file will be returned regardless of whether a violation has
    already been reported for the constant or if it has been marked as deprecated.

--- test/unit/node_visitor_test.rb
+++ test/unit/node_visitor_test.rb
@@ -8,7 +8,7 @@ module Packwerk
   class NodeVisitorTest < Minitest::Test
     test "#visit visits the correct number of nodes" do
       node_processor = mock
-      node_processor.expects(:call).times(3).returns("an offense")
+      node_processor.expects(:call).times(3).returns(["an offense"])
       file_node_visitor = Packwerk::NodeVisitor.new(node_processor: node_processor)
 
       node = ParserTestHelper.parse("class Hello; world; end")
