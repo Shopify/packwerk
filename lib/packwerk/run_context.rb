d29de0cf5c5234fc00987f1d8809f1d35934ccfe
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-08-11 09:07:26 -0600

    Simplify packwerk path determination mechanics

--- lib/packwerk/run_context.rb
+++ lib/packwerk/run_context.rb
@@ -38,7 +38,7 @@ module Packwerk
     sig do
       params(
         root_path: String,
-        load_paths: T::Hash[String, Module],
+        load_paths: T::Array[String],
         inflector: T.class_of(ActiveSupport::Inflector),
         cache_directory: Pathname,
         config_path: T.nilable(String),
