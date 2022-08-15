1af42ebe2f9c8030f7ae08e53a31a3c547249d47
Rafael Mendonça França <rafael.franca@shopify.com>
2022-08-15 16:36:19 +0000

    Change the Ruby parser to use syntax_tree

--- lib/packwerk/parsers/ruby.rb
+++ lib/packwerk/parsers/ruby.rb
@@ -1,41 +1,20 @@
 # typed: true
 # frozen_string_literal: true
 
-require "parser"
-require "parser/current"
+require "syntax_tree"
 
 module Packwerk
   module Parsers
     class Ruby
       include ParserInterface
 
-      class RaiseExceptionsParser < Parser::CurrentRuby
-        def initialize(builder)
-          super(builder)
-          super.diagnostics.all_errors_are_fatal = true
-        end
-      end
-
-      class TolerateInvalidUtf8Builder < Parser::Builders::Default
-        def string_value(token)
-          value(token)
-        end
-      end
-
-      def initialize(parser_class: RaiseExceptionsParser)
-        @builder = TolerateInvalidUtf8Builder.new
+      def initialize(parser_class: SyntaxTree)
         @parser_class = parser_class
       end
 
       def call(io:, file_path: "<unknown>")
-        buffer = Parser::Source::Buffer.new(file_path)
-        buffer.source = io.read
-        parser = @parser_class.new(@builder)
-        parser.parse(buffer)
-      rescue EncodingError => e
-        result = ParseResult.new(file: file_path, message: e.message)
-        raise Parsers::ParseError, result
-      rescue Parser::SyntaxError => e
+        @parser_class.parse(io.read)
+      rescue SyntaxTree::Parser::ParseError => e
         result = ParseResult.new(file: file_path, message: "Syntax error: #{e}")
         raise Parsers::ParseError, result
       end
--- test/unit/parsers/ruby_test.rb
+++ test/unit/parsers/ruby_test.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "packwerk/parsers/ruby"
 
 module Packwerk
   module Parsers
@@ -11,49 +12,22 @@ module Packwerk
           Ruby.new.call(io: fixture)
         end
 
-        assert_kind_of(::AST::Node, node)
+        assert_kind_of(SyntaxTree::Node, node)
       end
 
       test "#call writes parse error to stdout" do
-        error_message = "stub error"
-        err = Parser::SyntaxError.new(stub(message: error_message))
-        parser = stub
-        parser.stubs(:parse).raises(err)
-
-        parser_class_stub = stub(new: parser)
-
-        parser = Ruby.new(parser_class: parser_class_stub)
         file_path = fixture_path("invalid.rb")
 
-        exc = assert_raises(Parsers::ParseError) do
+        exception = assert_raises(Parsers::ParseError) do
           File.open(file_path, "r") do |fixture|
-            parser.call(io: fixture, file_path: file_path)
+            Ruby.new.call(io: fixture, file_path: file_path)
           end
         end
 
-        assert_equal("Syntax error: stub error", exc.result.message)
-        assert_equal(file_path, exc.result.file)
-      end
-
-      test "#call writes encoding error to stdout" do
-        error_message = "stub error"
-        err = EncodingError.new(error_message)
-        parser = stub
-        parser.stubs(:parse).raises(err)
-
-        parser_class_stub = stub(new: parser)
-
-        parser = Ruby.new(parser_class: parser_class_stub)
-        file_path = fixture_path("invalid.rb")
-
-        exc = assert_raises(Parsers::ParseError) do
-          File.open(file_path, "r") do |fixture|
-            parser.call(io: fixture, file_path: file_path)
-          end
-        end
-
-        assert_equal("stub error", exc.result.message)
-        assert_equal(file_path, exc.result.file)
+        assert_equal(
+          "Syntax error: syntax error, unexpected end-of-input, expecting `end' in #{file_path}",
+          exception.message
+        )
       end
 
       test "#call parses Ruby code containing invalid UTF-8 strings" do
@@ -61,7 +35,7 @@ module Packwerk
           Ruby.new.call(io: fixture)
         end
 
-        assert_kind_of(::AST::Node, node)
+        assert_kind_of(SyntaxTree::Node, node)
       end
 
       private
