06e173f509a384cc8e283fa0561d2b8139793985
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2022-11-04 20:28:05 -0500

    Fix constant redefinition errors when stubbing constants

--- test/support/stub_const.rb
+++ test/support/stub_const.rb
@@ -1,12 +1,25 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module StubConst
-  def with_stubbed_const(klass, const, val)
-    original_value = klass.const_get(const) # rubocop:disable Sorbet/ConstantsFromStrings
-    klass.const_set(const, val)
-    yield
-  ensure
-    klass.const_set(const, original_value)
+  extend T::Sig
+
+  sig do
+    params(
+      mod: Module,
+      const: T.any(Symbol, String),
+      value: Object,
+      block: T.proc.void
+    ).void
+  end
+  def with_stubbed_const(mod, const, value, &block)
+    original_value = mod.const_get(const) # rubocop:disable Sorbet/ConstantsFromStrings
+
+    Kernel.silence_warnings do
+      mod.const_set(const, value)
+      yield
+    ensure
+      mod.const_set(const, original_value)
+    end
   end
 end
