f5d5af4bc7cf31e182aef59433f40fed9dd86b25
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2022-11-07 15:25:25 -0600

    Require Zeitwerk 2.6.1
    Require Zeitwerk 2.6.1 so that we can always use the public
    introspection API to get application autoload paths.

--- lib/packwerk/application_load_paths.rb
+++ lib/packwerk/application_load_paths.rb
@@ -20,7 +20,7 @@ module Packwerk
 
       sig { returns(T::Hash[String, Module]) }
       def extract_application_autoload_paths
-        Loader.autoloaders.inject({}) do |h, loader|
+        Rails.autoloaders.inject({}) do |h, loader|
           h.merge(loader.dirs(namespaces: true))
         end
       end
