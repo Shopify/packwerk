24e26b9fbf6a58a4d94169554ff7ed6d7f4578b1
David Stosik <david.stosik@shopify.com>
2023-04-12 12:53:21 +0000

    Copy parse_run method to UpdateCommand

--- lib/packwerk/cli.rb
+++ lib/packwerk/cli.rb
@@ -75,7 +75,7 @@ module Packwerk
       when "check"
         CheckCommand.new(self, args).run
       when "update-todo", "update"
-        UpdateCommand.new(parse_run: parse_run(args)).run
+        UpdateCommand.new(self, args).run
       when "validate"
         ValidateCommand.new(
           configuration: @configuration,
@@ -99,63 +99,6 @@ module Packwerk
         @out.puts(result.message)
       end
       result.status
-    end
-
-    private
-
-    sig do
-      params(
-        relative_file_paths: T::Array[String],
-        ignore_nested_packages: T::Boolean
-      ).returns(FilesForProcessing)
-    end
-    def fetch_files_to_process(relative_file_paths, ignore_nested_packages)
-      files_for_processing = FilesForProcessing.fetch(
-        relative_file_paths: relative_file_paths,
-        ignore_nested_packages: ignore_nested_packages,
-        configuration: @configuration
-      )
-      @out.puts(<<~MSG.squish) if files_for_processing.files.empty?
-        No files found or given.
-        Specify files or check the include and exclude glob in the config file.
-      MSG
-
-      files_for_processing
-    end
-
-    sig { params(args: T::Array[String]).returns(ParseRun) }
-    def parse_run(args)
-      relative_file_paths = T.let([], T::Array[String])
-      ignore_nested_packages = T.let(false, T::Boolean)
-      formatter = @offenses_formatter
-
-      OptionParser.new do |parser|
-        parser.on("--packages=PACKAGESLIST", Array, "package names, comma separated") do |p|
-          relative_file_paths = p
-          ignore_nested_packages = true
-        end
-
-        parser.on("--offenses-formatter=FORMATTER", String,
-          "identifier of offenses formatter to use") do |formatter_identifier|
-          formatter = OffensesFormatter.find(formatter_identifier)
-        end
-
-        parser.on("--[no-]parallel", TrueClass, "parallel processing") do |parallel|
-          @configuration.parallel = parallel
-        end
-      end.parse!(args)
-
-      relative_file_paths = args if relative_file_paths.empty?
-
-      files_for_processing = fetch_files_to_process(relative_file_paths, ignore_nested_packages)
-
-      ParseRun.new(
-        relative_file_set: files_for_processing.files,
-        file_set_specified: files_for_processing.files_specified?,
-        configuration: @configuration,
-        progress_formatter: @progress_formatter,
-        offenses_formatter: formatter
-      )
     end
   end
 end
--- lib/packwerk/cli/update_command.rb
+++ lib/packwerk/cli/update_command.rb
@@ -6,14 +6,72 @@ module Packwerk
     class UpdateCommand
       extend T::Sig
 
-      sig { params(parse_run: ParseRun).void }
-      def initialize(parse_run:)
-        @parse_run = parse_run
+      sig { params(cli: Cli, args: T::Array[String]).void }
+      def initialize(cli, args)
+        @cli = cli
+        @args = args
       end
 
       sig { returns(Result) }
       def run
-        @parse_run.update_todo
+        parse_run.update_todo
+      end
+
+      private
+
+      sig do
+        params(
+          relative_file_paths: T::Array[String],
+          ignore_nested_packages: T::Boolean
+        ).returns(FilesForProcessing)
+      end
+      def fetch_files_to_process(relative_file_paths, ignore_nested_packages)
+        files_for_processing = FilesForProcessing.fetch(
+          relative_file_paths: relative_file_paths,
+          ignore_nested_packages: ignore_nested_packages,
+          configuration: @cli.configuration
+        )
+        @cli.out.puts(<<~MSG.squish) if files_for_processing.files.empty?
+          No files found or given.
+          Specify files or check the include and exclude glob in the config file.
+        MSG
+
+        files_for_processing
+      end
+
+      sig { returns(ParseRun) }
+      def parse_run
+        relative_file_paths = T.let([], T::Array[String])
+        ignore_nested_packages = T.let(false, T::Boolean)
+        formatter = @cli.offenses_formatter
+
+        OptionParser.new do |parser|
+          parser.on("--packages=PACKAGESLIST", Array, "package names, comma separated") do |p|
+            relative_file_paths = p
+            ignore_nested_packages = true
+          end
+
+          parser.on("--offenses-formatter=FORMATTER", String,
+            "identifier of offenses formatter to use") do |formatter_identifier|
+            formatter = OffensesFormatter.find(formatter_identifier)
+          end
+
+          parser.on("--[no-]parallel", TrueClass, "parallel processing") do |parallel|
+            @cli.configuration.parallel = parallel
+          end
+        end.parse!(@args)
+
+        relative_file_paths = @args if relative_file_paths.empty?
+
+        files_for_processing = fetch_files_to_process(relative_file_paths, ignore_nested_packages)
+
+        ParseRun.new(
+          relative_file_set: files_for_processing.files,
+          file_set_specified: files_for_processing.files_specified?,
+          configuration: @cli.configuration,
+          progress_formatter: @cli.progress_formatter,
+          offenses_formatter: formatter
+        )
       end
     end
 
