8bddb6e353670319743e97f2df70244f79656e4b
Richard Marbach <rmarbach@gmail.com>
2024-01-08 07:56:27 +0100

    Remove ParserInterface from autoload target because it was deleted

--- lib/packwerk/parsers.rb
+++ lib/packwerk/parsers.rb
@@ -5,7 +5,6 @@ module Packwerk
   module Parsers
     autoload :Erb, "packwerk/parsers/erb"
     autoload :Factory, "packwerk/parsers/factory"
-    autoload :ParserInterface, "packwerk/parsers/parser_interface"
     autoload :Ruby, "packwerk/parsers/ruby"
 
     class ParseResult < Offense; end
